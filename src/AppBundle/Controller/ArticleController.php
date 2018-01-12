6a97409435f63a7bbf2307f7e5534469075c6771
tberranger <tberranger@hotmail.fr>
2018-01-12 11:39:58 +0100

    Home, Article list updated

---
+++ src/AppBundle/Controller/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * @Route("/article/list", name="article_list")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('article/list.html.twig');
+    }
+}
--- src/AppBundle/Controller/MenuController.php
+++ src/AppBundle/Controller/MenuController.php
@@ -6,16 +6,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class MenuController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('menu/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $homeNavbar = true;
+
+        return $this->render('menu/index.html.twig', array(
+            'homeNavbar' => $homeNavbar
+        ));
     }
 }
